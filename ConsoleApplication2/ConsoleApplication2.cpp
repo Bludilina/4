caaccbafd4eed5ced5f5dc08716b94b24055f9d7
Bludilina <90140085+Bludilina@users.noreply.github.com>
2022-06-17 08:16:39 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <queue>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void printQueue(queue<char>& inputQueue)
+{
+    while (!inputQueue.empty())
+    {
+        cout << inputQueue.front();
+        inputQueue.pop();
+    }
+}
+
+int main()
+{
+    ifstream fin("input.txt");
+    string buf;
+    queue<char> digitsQueue, lettersQueue, otherQueue;
+
+    getline(fin, buf);
+    while (!fin.eof())
+    {
+        for (char symbol : buf)
+        {
+            if (isdigit(symbol))
+                digitsQueue.push(symbol);
+            else if (isalpha(symbol))
+                lettersQueue.push(symbol);
+            else
+                otherQueue.push(symbol);
+        }
+        getline(fin, buf);
+    }
+
+    cout << "Digits: ";
+    printQueue(digitsQueue);
+    cout << endl;
+
+    cout << "Letters: ";
+    printQueue(lettersQueue);
+    cout << endl;
+
+    cout << "Other: ";
+    printQueue(otherQueue);
+    cout << endl;
+
+}
